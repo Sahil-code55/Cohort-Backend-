b56f4e48d57ee9b9f8acc23737fea89421ac723d
Sahil Sahu <sahusahil553@gmail.com>
2026-09-11 00:11:38 +0530

    frontend design done

--- 05-Form-Data/Client/src/App.jsx
+++ 05-Form-Data/Client/src/App.jsx
@@ -11,15 +11,28 @@ const SubmitHandler = (data)=>{
 }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold text-red-500">Tailwind works</h1>
+    <div className="h-screen flex justify-center bg-gray-600 items-center">
+    
     <form
-    className="flex flex-col gap-4 "
+    className="flex flex-col bg-slate-100 p-4 rounded-xl gap-4 "
      onSubmit={handleSubmit((SubmitHandler))}  action="">
-      <input type="text" placeholder=" Enter your name " />
-      <input type="text" placeholder=" Enter your email " />
-      <input type="file" placeholder=" upload your profile pic " />
-      <input type="submit" />
+
+      <input 
+      {...register("name")}
+      className="border border-gray-400 rounded-xl p-2 text-gray-600 hover:border-blue-400" type="text" placeholder=" Enter your name " />
+
+      <input
+       {...register("email")}
+       className="border border-gray-400 rounded-xl p-2 text-gray-600 hover:border-blue-400" type="text" placeholder=" Enter your email " />
+
+      <input 
+       {...register("profilePic")}
+      className="border border-gray-400 rounded-xl p-2 text-gray-600 hover:border-blue-400" type="file" placeholder=" upload your profile pic " />
+
+      <input 
+      className="px-5 py-3 bg-blue-500 rounded-3xl
+       hover:bg-blue-400 active:scale-95"
+       type="submit" />
     </form>
     </div>
   )
